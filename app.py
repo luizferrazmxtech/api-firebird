9ffc34f615a2190a19daf1e59b0f298f9b497cdc
luizferrazmxtech <luizferraz@mxtech.inf.br>
2025-06-21 21:01:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ def home():
   <style>
     body { font-family: Arial, sans-serif; margin: 0; background: #f8f8f8; }
     header { background: #f0f0f0; padding: 30px; text-align: center; }
-    header img {height: 200px; display: block; margin: 0 auto;}
+    header img {height: 100px; display: block; margin: 0 auto;}
     .container { max-width: 400px; margin: 40px auto; background: #fff; padding: 20px; border-radius: 8px; }
     label, input, button { display: block; width: 100%; margin-bottom: 10px; }
     input { padding: 8px; border: 1px solid #ccc; border-radius: 4px; }
@@ -119,7 +119,7 @@ def home():
     <label for="nrorc">Número do Orçamento:</label>
     <input id="nrorc" name="nrorc" required>
     <button type="submit" name="format" value="html">Visualizar HTML</button>
-    <button type="submit" name="format" value="pdf">Download PDF</button>
+    <button class="btn-pdf" type="submit" name="format" value="pdf">Download PDF</button>
   </form>
 </div>
 </body>
